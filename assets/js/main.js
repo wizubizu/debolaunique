249b47957c0131cd5ed6280a5db2bb70b27dda5b
wizubizu <odugbesanabimbola@gmail.com>
2021-07-01 04:39:54 +0100

    pop up for AD

--- assets/js/main.js
+++ assets/js/main.js
@@ -91,3 +91,30 @@ $(function() {
 	} );
 
 });
+
+
+
+// for Advert
+$(document).ready(function(){
+
+    $(window).load(function(){
+        $duration = 15;
+        $('.seconds').text($duration);
+        $('.popup-wrap').fadeIn(1500);
+
+        $myTimer = setInterval(function(){ startTimer() }, 1000);
+        $('.popup .btn-close').on("click",function(){
+            clearInterval($myTimer);
+            $('.popup-wrap').fadeOut(500);
+        });
+        
+        function startTimer(){
+            $duration--;
+            $('.seconds').text($duration);
+            if($duration==0){
+                clearInterval($myTimer);
+                $('.popup-wrap').fadeOut(500);
+            }
+        }
+    });
+});
