67665cfea778cbf1c4756b9eede185689ea47dd8
wizubizu <odugbesanabimbola@gmail.com>
2021-07-12 14:26:58 +0100

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -128,3 +128,23 @@ $(document).ready(function(){
     });
 
    // end
+
+
+
+
+   // accordion
+var acc = document.getElementsByClassName("__accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight){
+        panel.style.maxHeight = null;
+    } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        } 
+    });
+}
+// end
