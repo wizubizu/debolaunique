585bcadacd6013631a04c9752c21d2e6be2f8cb1
wizubizu <odugbesanabimbola@gmail.com>
2021-08-13 11:42:57 +0100

    hotfixed

--- assets/js/main.js
+++ assets/js/main.js
@@ -141,22 +141,6 @@ $(document).ready(function(){
 
 
 
-   // accordion
-var acc = document.getElementsByClassName("__accordion");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.maxHeight){
-        panel.style.maxHeight = null;
-    } else {
-            panel.style.maxHeight = panel.scrollHeight + "px";
-        } 
-    });
-}
-// end
 
 
 
